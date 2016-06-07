55aa97db6d4afca0906ac5570c069db30b20dd8b
EC2 Default User <ec2-user@ip-172-31-31-165.eu-central-1.compute.internal>
2016-06-07 17:37:24 +0000

    add mailing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import boto3, json, time
+from mail import send_email
 from uuid import uuid4
 from creator import create
 
@@ -8,17 +9,21 @@ def upload_s3(source_file, filename):
   destination_filename = "albums/%s/%s" % (uuid4().hex, filename)
   print destination_filename
   bucket = s3.Bucket(bucket_name)
-  bucket.put_object(Key=destination_filename, Body=source_file)
+  bucket.put_object(Key=destination_filename, Body=source_file, ACL='public-read')
+  return destination_filename
 
 sqs = boto3.resource('sqs')
 albumRequests = sqs.get_queue_by_name(QueueName='kanclerj-album')
+bucket_address = 'https://s3.eu-central-1.amazonaws.com/153412-kkanclerz'
 
 while True:
   for albumRequest in albumRequests.receive_messages():
     print('processing request ..........')
     albumData = json.loads(albumRequest.body)
     pdf = create(albumData)
-    upload_s3(pdf.getvalue(), 'album.pdf')
+    dest = upload_s3(pdf.getvalue(), 'album.pdf')
+    send_email(albumData['sent_to'], 'Your album', 'download here: %s/%s' % (
+    bucket_address, dest))
     albumRequest.delete()
     print('request processing finished [X]')
   time.sleep(1)
